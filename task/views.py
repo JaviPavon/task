5048c2eceb7b62dd059c2adee9863b8c6f7fe601
javipavon <xjpavbor068@ieshnosmachado.org>
2023-11-08 10:20:39 +0100

    Update

--- task/views.py
+++ task/views.py
@@ -4,7 +4,16 @@ from django.utils import timezone
 
 from .models import Task
 
+from .forms import TaskForm
+
 def list_tasks(request):
 
     tasks = Task.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
-    return render(request, 'task/list_tasks.html', {'tasks': tasks})
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form=TaskForm()
+    return render(request, 'task/list_tasks.html', {'tasks': tasks, 'form':form})
